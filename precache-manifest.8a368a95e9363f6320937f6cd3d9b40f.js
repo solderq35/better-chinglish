43d1cdcbee739e4c9bd7ad23fa1383cc5911d17e
solderq35 <82061589+solderq35@users.noreply.github.com>
2023-06-15 15:25:03 +0000

    Deploying to gh-pages from @ solderq35/better-chinglish@5980a57954bc25a475ed601f0c6d3e9bca2624c8 🚀

---
+++ precache-manifest.8a368a95e9363f6320937f6cd3d9b40f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753d2ea6e14e9af2eacfe72b0f97cb54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffb24467d938b67b0e69",
+    "url": "/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "733eadcb4ce613194a27",
+    "url": "/static/js/2.fa02c139.chunk.js"
+  },
+  {
+    "revision": "ffb24467d938b67b0e69",
+    "url": "/static/js/main.770cbeea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
